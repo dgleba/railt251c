ef1928bdc9cd24c03cdef83562cde2943584f17f
David Gleba <dgleba@gmail.com>
2016-09-08 19:28:44 -0400

    change kaminari lines per page

--- variants/ransack-kaminari/template.rb
+++ variants/ransack-kaminari/template.rb
@@ -5,3 +5,9 @@ apply 'variants/ransack-kaminari/lib/template.rb'
 apply 'variants/ransack-kaminari/spec/template.rb'
 
 generate 'kaminari:config'
+
+
+# set records per page..
+gsub_file 'config/initializers/kaminari_config.rb',
+          '# config.default_per_page = 25',
+          'config.default_per_page = 10'
