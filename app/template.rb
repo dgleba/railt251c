55b2548625a880076c89e3f0bd3d44fde2dc4f90
David Gleba <dgleba@gmail.com>
2016-09-08 18:09:41 -0400

    disable draper

--- app/template.rb
+++ app/template.rb
@@ -1,5 +1,5 @@
 apply 'app/assets/template.rb'
-apply 'app/controllers/application_controller.rb'
+# apply 'app/controllers/application_controller.rb'
 
 remove_file 'app/views/layouts/application.html.erb'
 template 'app/views/layouts/application.html.haml.tt',
---
+++ readme-status.txt
@@ -0,0 +1,4 @@
+
+2016-09-07_Wed_09.25-AM removing  draper 
+
+#
