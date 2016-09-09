da708639c0fd0d8a57636ca4836acdb4bcbb7fc7
David Gleba <dgleba@gmail.com>
2016-09-08 20:48:48 -0400

    change index template

--- variants/ransack-kaminari/lib/templates/haml/scaffold/index.html.haml.rb
+++ variants/ransack-kaminari/lib/templates/haml/scaffold/index.html.haml.rb
@@ -3,13 +3,17 @@ if apply_twitter_bootstrap?
     <<-'RUBY'
 
 .row
-  .col-sm-5.col-sm-offset-7
+  .col-sm-2
+    = link_to t('actions.create'), new_customer_path, class: 'btn btn-primary'
+  .col-sm-3
     = search_form_for @q, class: 'search-form' do |f|
       .input-group
         = f.search_field :<%= attributes.first.name %>_cont, class: 'form-control'
         %span.input-group-btn
           %button.btn.btn-primary
             %i.fa.fa-search
+= paginate @<%= plural_table_name %>
+
   RUBY
   end
 else
@@ -28,10 +32,3 @@ gsub_file 'lib/templates/haml/scaffold/index.html.haml',
           '%th= <%= class_name %>.human_attribute_name :<%= attribute.name %>',
           '%th= sort_link(@q, :<%= attribute.name %>, <%= class_name %>.human_attribute_name(:<%= attribute.name %>))'
 
-insert_into_file 'lib/templates/haml/scaffold/index.html.haml', before: /^= link_to t('actions.create')*/ do
-  <<-'RUBY'
-
-= paginate @<%= plural_table_name %>
-
-  RUBY
-end
